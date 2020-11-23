b63776ef7f39f0c7a991fc9be8fc65dfa5f9f3ec
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-11-23 02:10:21 +0100

    LogIn, Dashboard Done, no routes

---
+++ src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import NavbarComp from "./NavbarComp.jsx";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      view : "main",
+    }
+    this.changeView = this.changeView.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  }
+  changeView(e) {
+    this.setState({ view: e.target.value });
+  }
+  
+  render() {
+    if (this.state.view === "main") {
+      return (
+        <div>
+          <center>
+            <br></br> <br></br>
+           <form id="form">
+           <h1>Hello Dear Employee </h1>
+           <input type="text" placeholder="email" onChange={(e)=>this.setState({email:e.target.value})} value={this.state.email}/><br></br> <br></br>
+
+             <input type="password" placeholder="Password" onChange={(e)=>this.setState({password:e.target.value})} value={this.state.password}/><br></br> <br></br>
+
+             
+             <input type="button" value="Login" onClick={this.changeView} /><br></br> <br></br>
+             
+           </form>
+          </center>
+        </div>
+      )
+    } else if (this.state.view ==="Login" ) {
+      return (
+        <div>
+      <NavbarComp />
+      </div>
+      )
+    }
+  }
+}
+    
+  
+
+export default Login;
