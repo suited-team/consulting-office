3d187682c8c15d914b0bd08224283af5cff8c892
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-11-24 21:45:26 +0100

    LogOut

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -7,6 +7,8 @@ import Dashboard from'./Dashboard.jsx'
 import './NavbarComp.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/links.css';
+import Login from'./Login.jsx';
+
 
 
 import {
@@ -33,11 +35,12 @@ class NavbarComp extends React.Component {
 
     <Link className="links" to="/Dashboard">Dashboard</Link>
    <Link className="links" to="/MsgService">MsgService</Link>
- 
+
      
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
+    <Link className="links" to="/Login">Logout</Link>
+
       
     </Nav>
   </Navbar.Collapse>
@@ -48,6 +51,8 @@ class NavbarComp extends React.Component {
 <Switch>
     <Route exact path="/Dashboard" component={Dashboard} />
     <Route exact path="/MsgService" component={MsgService} />
+    <Route exact path="/Login" component={Login} />
+
   </Switch>
 
 </Router>
