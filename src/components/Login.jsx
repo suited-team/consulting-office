45bc7ca74824a7f4ef2dd16f42d439601b17b2de
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-11-24 16:27:36 +0100

    2nd step CSS

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,28 +25,51 @@ class Login extends Component {
     if (this.state.view === "main") {
       return (
         
-        <div>
-          <center>
-            <div className ="login-wrap">
-              <div className = "login-html">
-            <br></br> <br></br>
-           <form id="form" className ="login-form">
+        
 
-           <h1>Hello Dear Employee </h1>
-
-
-          <input className ="input" type="text" placeholder="email" onChange={(e)=>this.setState({email:e.target.value})} value={this.state.email}/><br></br> <br></br>
-
-          <input className ="input" type="password" placeholder="Password" onChange={(e)=>this.setState({password:e.target.value})} value={this.state.password}/><br></br> <br></br>
-
-          <input type="button" value="Login" onClick={this.props.changeView} /><br></br> <br></br>
-             
-           </form>
-           </div>
-           </div>
-          </center>
+<div className="container">
+  <div className="d-flex justify-content-center h-100">
+    <div className="card">
+      <div className="card-header">
+       <h3>Sign In</h3>
+        <div class="d-flex justify-content-end social_icon">
+          <span><i class="fab fa-facebook-square"></i></span>
+          <span><i class="fab fa-google-plus-square"></i></span>
+          <span><i class="fab fa-twitter-square"></i></span>
         </div>
-      )
+      </div>
+
+
+    <div className="card-body">
+      <form>
+        <div className="input-group form-group">
+          <div className="input-group-prepend">
+              <span className="input-group-text"><i class="fas fa-user"></i></span>
+          </div>
+             <input type="text" className="form-control" type="password" placeholder="Password" onChange={(e)=>this.setState({password:e.target.value})} value={this.state.password} placeholder="username"/>
+          
+           </div>
+          <div className="input-group form-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text"><i class="fas fa-key"></i></span>
+            </div>
+              <input type="password" className="form-control"  input className ="input" type="password" placeholder="Password" onChange={(e)=>this.setState({password:e.target.value})} value={this.state.password} placeholder="password"/>
+          </div>
+
+          <div className="form-group">
+            <input type="submit" onClick={this.props.changeView} value="Login" className="btn float-right login_btn"/>
+          </div>
+
+      </form>
+      </div>
+
+
+
+    
+  </div>
+</div>
+</div>
+)
     } else if (this.state.view ==="Login" ) {
       return (
         <div>
