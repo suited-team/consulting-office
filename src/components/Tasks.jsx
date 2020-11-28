207ff6e713147b58778d723ecf6bf55f7322df19
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-11-28 16:39:27 +0100

    ahmed request

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import Dropdown from 'react-bootstrap/DropDown';
+
+
+
+
+class Tasks extends Component {
+    render() {
+
+return(
+    <div>
+<Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Description of the Task</th>
+      <th>Client</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td><Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Status
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">In Progress</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">On Hold</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Complete</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td><Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Status
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">In Progress</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">On Hold</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Complete</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td><Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Status
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">In Progress</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">On Hold</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Complete</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</td>
+    </tr>
+  </tbody>
+</Table>
+</div>
+)
+}
+};
+
+export default Tasks;
