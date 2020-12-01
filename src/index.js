6b319c35122f6d3e0fa5df85d9842d03b12acb02
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-12-01 10:50:59 +0100

    small changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
-import {
-    Link
-} from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
