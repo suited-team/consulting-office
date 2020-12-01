ed4149d2f19392b6087177c674bca0681ceb9d17
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-12-01 10:44:45 +0100

    part 3

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Table from 'react-bootstrap/Table';
-import Dropdown from 'react-bootstrap/DropDown';
 
 
 
