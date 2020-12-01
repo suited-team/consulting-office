d397706712002b71a144af99a4dac3b37150a139
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-12-01 10:39:34 +0100

    resolved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ class App extends Component {
             </Router>):(
             <Router>
               <Switch>
-                <Route path="/" exact exact component={Login} />
+                <Route path="/" exact  component={Login} />
               </Switch>
             </Router>
             )}
