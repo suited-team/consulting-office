572326eccec0920dccc81b163166365694038e19
Issam Ben Mansour <issambenmansour@hotmail.com>
2020-12-01 10:49:43 +0100

    principle changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import {
-    Route,
     Link
 } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
