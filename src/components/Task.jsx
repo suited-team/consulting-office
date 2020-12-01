692c8f452c012c36e7f46dd6cc81204f3f5b9631
amirabourogaa <68273575+amirabourogaa@users.noreply.github.com>
2020-12-01 06:36:27 -0800

    changes made in the admin login

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import TaskM from "./Tasks";
-
+import '../components/Task.css';
+import Table from "react-bootstrap/Table";
+import { Dropdown } from 'react-bootstrap';
 class Task extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +12,11 @@ class Task extends Component {
   }
 
   check() {
-    axios.get("http://localhost:5500/task").then((response) => {
+    let obj={
+      email:localStorage.getItem('emailEmployee')
+    }
+
+    axios.get("https://server-cunsulting.herokuapp.com/task/Employee").then((response) => {
       //need to change the link
       if (response.data.length > this.state.data.length) {
         this.setState({ data: response.data });
@@ -24,53 +29,53 @@ class Task extends Component {
     console.log(this.state.data);
 
     return (
-      <div className="container">
-        <table class="table table-sm table-dark">
+      <div style={{marginTop:'100px',marginLeft:'200px'}}>
+        
+        
+          <Table
+          style={{width:'1000px',fontSize:'20px'}}
+          striped
+          bordered
+          hover
+          variant="dark">
           <thead>
             <tr>
-              <th className="bg-info" scope="col">
-                In progress
-              </th>
-              <th className="bg-danger" scope="col">
-                On hold
-              </th>
-              <th className="bg-warning" scope="col">
-                In progress
-              </th>
-              <th className="bg-success" scope="col">
-                Done
-              </th>
+              <th>ID</th>
+              <th>Description of the Task</th>
+              <th>Client name</th>
+              <th>Employee name</th>
+              <th>Creation date</th>
+              <th>Due date</th>
+              <th>status</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="bg-info">IP</td>
-              <td className="bg-danger">OH</td>
-              <td className="bg-warning">IP</td>
-              <td className="bg-success">done</td>
-            </tr>
-          </tbody>
-        </table>
+          {this.state.data.map((t, i) => (
+              <tr>
+                <td>{i + 1}</td>
+                <td>descp </td>
+                <td>{t.ClientName}</td>
+                <td>{t.EmployeeName}</td>
+                <td>{t.startDate}</td>
+                <td>{t.DueDate}</td>
+                <td>
+                <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Change status
+  </Dropdown.Toggle>
 
-        {this.state.data.map((element, index) => {
-          return element.status === "fase 1" ? (
-            <div className="phase1">
-              <TaskM task={element} key={index} />
-            </div>
-          ) : element.status === "fase 2" ? (
-            <div className="phase2">
-              <TaskM task={element} key={index} />
-            </div>
-          ) : element.status === "fase 3" ? (
-            <div className="phase3">
-              <TaskM task={element} key={index} />{" "}
-            </div>
-          ) : (
-            <div className="phase4">
-              <TaskM task={element} key={index} />{" "}
-            </div>
-          );
-        })}
+  <Dropdown.Menu>
+    <Dropdown.Item style={{backgroundColor:'#387DF8',color:'white'}}>In progress</Dropdown.Item>
+    <Dropdown.Item style={{backgroundColor:'red',color:'white'}} >On hold</Dropdown.Item>
+    <Dropdown.Item style={{backgroundColor:'green',color:'white'}}>Done</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+                </td>
+              </tr>
+            ))}
+         
+          </tbody>
+        </Table>
       </div>
     );
   }
